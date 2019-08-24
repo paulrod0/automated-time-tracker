6b603e707dca8087d83ffd89dab0f8be498898d7
Pablo Lopez <pablo.lprdz>
2019-08-24 11:29:50 +0200

    update

--- autotimer.py
+++ autotimer.py
@@ -1,6 +1,7 @@
+from __future__ import print_function
 from AppKit import NSWorkspace
 import time
-
+from foundation import *
 active_window_name = ""
 while True:
 
@@ -10,7 +11,7 @@ while True:
     if active_window_name != new_window_name:
         active_window_name = new_window_name
         print(active_window_name)
-        if active_window_name = 'Google Chrome':
+        if active_window_name = 'Microsoft Edge Beta':
             textOfMyscript = """tell app "microsoft edge beta" to get the url of the active tab of window 1"""
             s = NSAppleScript.initWithSource_(NSAppleScript.alloc(), textOfMyscript)
             results, err = s.executeAndReturnError_(None)
